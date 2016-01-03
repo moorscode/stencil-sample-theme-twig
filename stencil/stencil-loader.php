7bf11227a00316f96be6f92b6f2f032bc6b14ea2
Jip (desktop) <jhp.moors@gmail.com>
2016-01-03 19:31:25 +0100

    bootstrap - only bootstrap once

--- stencil/stencil-loader.php
+++ stencil/stencil-loader.php
@@ -6,8 +6,12 @@
  * @submodule Bootstrap
  */
 
-$implementations = new DirectoryIterator( __DIR__ );
+/**
+ * Only load if Stencil hasn't been loaded as plugin yet.
+ */
+$loaded_before = class_exists( 'Stencil_Bootstrap', false );
 
+$implementations = new DirectoryIterator( __DIR__ );
 foreach ( $implementations as $implementation ) {
 	if ( $implementation->isDir() && ! $implementation->isDot() ) {
 		require_once sprintf( '%1$s/%1$s.php', $implementation );
